4416aed3d42a7e2ecf8b7ff8298c01ef7b8ac299
Kitsune Solar <kitsune.solar@gmail.com>
2018-06-23 21:31:56 +0300

    Update resource

--- MW_EXT_Meta.class.php
+++ MW_EXT_Meta.class.php
@@ -1,42 +1,14 @@
 <?php
 
-namespace MediaWiki\Extension\Description2;
+namespace MediaWiki\Extension\MW_EXT_Meta;
 
-use OutputPage, RequestContext, Skin;
+use OutputPage, Skin;
+use MediaWiki\Extension\MW_EXT_Core\MW_EXT_Core;
 
 /**
  * Class MW_EXT_Meta
  * ------------------------------------------------------------------------------------------------------------------ */
 class MW_EXT_Meta {
-
-	/**
-	 * Get MediaWiki message.
-	 *
-	 * @param $string
-	 *
-	 * @return string
-	 */
-	private static function getMsgText( $string ) {
-		$outString = wfMessage( 'mw-ext-license-' . $string )->inContentLanguage()->text();
-
-		return $outString;
-	}
-
-	/**
-	 * Get configuration parameters.
-	 *
-	 * @param $config
-	 *
-	 * @return mixed
-	 * @throws \ConfigException
-	 * -------------------------------------------------------------------------------------------------------------- */
-
-	private static function getConfig( $config ) {
-		$context   = RequestContext::getMain()->getConfig();
-		$getConfig = $context->get( $config );
-
-		return $getConfig;
-	}
 
 	/**
 	 * Load resource function.
@@ -50,7 +22,7 @@ class MW_EXT_Meta {
 
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
 		$out->addStyle( 'https://fonts.googleapis.com/css?family=Fira+Mono:400,700|Roboto+Condensed:400,400i,700,700i|Roboto:400,400i,700,700i&amp;subset=cyrillic', 'screen' );
-		$out->addStyle( 'https://use.fontawesome.com/releases/v' . self::getConfig( 'EXT_Meta_FA_Version' ) . '/css/all.css', 'screen' );
+		$out->addStyle( 'https://use.fontawesome.com/releases/v' . MW_EXT_Core::getConfig( 'EXT_Meta_FA_Version' ) . '/css/all.css', 'screen' );
 		$out->addStyle( '/extensions/MW_EXT_Meta/modules/styles/theme.css', 'screen' );
 		$out->addModules( [ 'ext.mw.meta' ] );
 

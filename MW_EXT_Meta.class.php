a66b082d5275d411daea81227ded55234f96add1
Kitsune Solar <kitsune.solar@gmail.com>
2018-07-20 10:18:50 +0300

    2018-07-20 07:18:50

--- MW_EXT_Meta.class.php
+++ MW_EXT_Meta.class.php
@@ -3,7 +3,7 @@
 namespace MediaWiki\Extension\MW_EXT_Meta;
 
 use OutputPage, Skin;
-use MediaWiki\Extension\MW_EXT_Core\MW_EXT_Core;
+use MediaWiki\Extension\MW_EXT_Kernel\MW_EXT_Kernel;
 
 /**
  * Class MW_EXT_Meta
@@ -21,7 +21,7 @@ class MW_EXT_Meta {
 	 */
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
 		$out->addStyle( 'https://fonts.googleapis.com/css?family=Roboto+Condensed:400,400i,700,700i|Roboto:400,400i,700,700i|Fira+Mono:400,700&amp;subset=cyrillic', 'screen' );
-		$out->addStyle( 'https://use.fontawesome.com/releases/v' . MW_EXT_Core::getConfig( 'EXT_Meta_FA_Version' ) . '/css/all.css', 'screen' );
+		$out->addStyle( 'https://use.fontawesome.com/releases/v' . MW_EXT_Kernel::getConfig( 'EXT_Meta_FA_Version' ) . '/css/all.css', 'screen' );
 		$out->addStyle( '/extensions/MW_EXT_Meta/modules/styles/theme.css', 'screen' );
 		$out->addModules( [ 'ext.mw.meta' ] );
 
